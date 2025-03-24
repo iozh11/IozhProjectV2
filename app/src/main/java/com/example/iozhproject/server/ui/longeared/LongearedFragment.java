130d0e74d8036c30ead35713d3f46605357b070c
iozh11 <135107119+iozh11@users.noreply.github.com>
2025-03-24 06:45:33 +0300

    4.0
    готовый проект без входа в аккаунт

--- app/src/main/java/com/example/iozhproject/server/ui/longeared/LongearedFragment.java
+++ app/src/main/java/com/example/iozhproject/server/ui/longeared/LongearedFragment.java
@@ -4,30 +4,61 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.iozhproject.databinding.FragmentAfricanHedgehogBinding;
 import com.example.iozhproject.databinding.FragmentLongearedBinding;
+import com.example.iozhproject.server.ui.african.AfricanModel;
 
 
 public class LongearedFragment extends Fragment {
 
     private FragmentLongearedBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        LongearedModel longearedViewModel =
-                new ViewModelProvider(this).get(LongearedModel.class);
-
+        LongearedModel longearedModel =
+                new ViewModelProvider(this,
+                        new ViewModelProvider.Factory() {
+                            @NonNull
+                            @Override
+                            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                                if (modelClass.isAssignableFrom(LongearedModel.class)) {
+                                    return (T) new LongearedModel(requireContext());
+                                }
+                                throw new IllegalArgumentException();
+                            }
+                        }).get(LongearedModel.class);
         binding = FragmentLongearedBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textLongeared;
-        longearedViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button buttonLifestyle = binding.buttonLifestyleLongeared;
+        Button buttonLiving = binding.buttonLivingLongeared;
+        Button buttonFacts = binding.buttonFactsLongeared;
+
+        buttonLifestyle.setOnClickListener(v -> toggleSection(binding.textLifestyleLongeared));
+        buttonLiving.setOnClickListener(v -> toggleSection(binding.textLivingLongeared));
+        buttonFacts.setOnClickListener(v -> toggleSection(binding.textFactsLongeared));
+
+        final TextView textView = binding.textLongearedHedgehog;
+        longearedModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
+    }
+
+    private void toggleSection(TextView textView) {
+        binding.textLifestyleLongeared.setVisibility(View.GONE);
+        binding.textLivingLongeared.setVisibility(View.GONE);
+        binding.textFactsLongeared.setVisibility(View.GONE);
+
+        textView.setVisibility(textView.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
     }
 
     @Override
--- app/src/main/java/com/example/iozhproject/server/ui/longeared/LongearedModel.java
+++ app/src/main/java/com/example/iozhproject/server/ui/longeared/LongearedModel.java
@@ -1,16 +1,20 @@
 package com.example.iozhproject.server.ui.longeared;
 
+import android.content.Context;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.iozhproject.R;
 
 public class LongearedModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public LongearedModel() {
+    public LongearedModel(Context context) {
         mText = new MutableLiveData<>();
-//        mText.setValue("This is slideshow fragment");
+        mText.setValue(context.getString(R.string.ab_iozh_longeared));
     }
 
     public LiveData<String> getText() {
